a07e209740719d43369575e8f5e1451ca809f147
Dmitry <pirogovds98@gmail.com>
2023-11-30 14:06:24 +0300

    add script for responding to requests via Hook

--- include/function.php
+++ include/function.php
@@ -4,3 +4,73 @@ function vardump($str){
     var_dump($str);
     echo "</pre>";
 }
+/* для отправки текстовых сообщений */
+function TG_sendMessage($getQuery) {
+    $ch = curl_init("https://api.telegram.org/bot". TG_TOKEN ."/sendMessage?" . http_build_query($getQuery));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    $res = curl_exec($ch);
+    curl_close($ch);
+
+    return $res;
+}
+
+/* для отправки изображений */
+function TG_sendPhoto($arrayQuery) {
+    $ch = curl_init('https://api.telegram.org/bot'. TG_TOKEN .'/sendPhoto');
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $arrayQuery);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    $res = curl_exec($ch);
+    curl_close($ch);
+
+    return $res;
+}
+
+/* для получения данных о файле */
+function TG_getFile($arrayQuery) {
+    $ch = curl_init("https://api.telegram.org/bot". TG_TOKEN ."/getFile?" . http_build_query($arrayQuery));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    $res = curl_exec($ch);
+    curl_close($ch);
+
+    return $res;
+}
+
+function list_files($path) {
+    if ($path[mb_strlen($path) - 1] != '/') {
+        $path .= '/';
+    }
+
+    $files = array();
+    $dh = opendir($path);
+    while (false !== ($file = readdir($dh))) {
+        if ($file != '.' && $file != '..' && !is_dir($path.$file) && $file[0] != '.') {
+            $files[] = $file;
+        }
+    }
+
+    closedir($dh);
+    return $files;
+}
+
+
+$data = file_get_contents('php://input');
+
+$arrDataAnswer = json_decode($data, true);
+
+
+$textMessage = mb_strtolower($arrDataAnswer["message"]["text"]);
+$chatId = $arrDataAnswer["message"]["chat"]["id"];
+
+if(!empty($arrDataAnswer["message"]["photo"])) {
+    $documentData = array_pop($arrDataAnswer["message"]["photo"]);
+}
+else if(!empty($arrDataAnswer["message"]["document"])) {
+    $documentData = array_pop($arrDataAnswer["message"]["document"]);
+}
--- способ через СURL/index.php
+++ способ через СURL/index.php
@@ -6,27 +6,117 @@ ini_set('display_startup_errors', 1);
 require ('include/const.php');
 require ('include/function.php');
 
-// 2-й способ, отправка через curl
 
-// Сообщение
-$textMessage = "Новое сообщение из формы";
+/* Проверка на ответ при нажатии на кнопки */
 
-$getQuery = [
-    "chat_id" => TG_USER_ID,
-//    "text" => $textMessage,
-//    "parse_mode" => "html",
-//    "reply_to_message_id" => 45
-    "message_id"  => 47,
-];
+if($arrDataAnswer["callback_query"]) {
+    $dataBut = $arrDataAnswer["callback_query"]["data"];
+    $textMessage = mb_strtolower($arrDataAnswer["callback_query"]["message"]["text"]);
+    $chatId = $arrDataAnswer["callback_query"]["message"]["chat"]["id"];
 
-$ch = curl_init("https://api.telegram.org/bot". TG_TOKEN ."/deleteMessage?" . http_build_query($getQuery)); //инициализируем работу
+    if($dataBut == "but_1") {
+        $arrayQuery = array(
+            'chat_id' => $chatId,
+            'text' => "Ты нажал на 'КНОПКА 1'",
+            'parse_mode' => "html",
+        );
+        TG_sendMessage($arrayQuery);
+    }
+    else if($dataBut == "but_2") {
+        $arrayQuery = array(
+            'chat_id' => $chatId,
+            'text' => "Ты нажал на 'КНОПКА 2'",
+            'parse_mode' => "html",
+        );
+        TG_sendMessage($arrayQuery);
+    }
+}
 
-// Передаю специальные параметры для отправки запроса
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_HEADER, false);
+if($textMessage == 'привет') {
+    $textMessage_bot = "Привет! Есть фото для меня?";
 
-$resultQuery = curl_exec($ch); //Записываю сам процесс работы curl_exec - отправляет запрос и возвращает данные, которые я записываю в данную переменную.
-curl_close($ch);// Закрываю соединения
+    $arrayQuery = array(
+        'chat_id' 		=> $chatId,
+        'text'			=> $textMessage_bot,
+        'parse_mode'	=> "html",
+    );
+    TG_sendMessage($arrayQuery);
+} else if($textMessage == 'хочу фото') {
+    $textMessage_bot = "Вот, держи!";
 
-echo $resultQuery;
+    $listFile = list_files(__DIR__ . "/img/");
+
+    $max = count($listFile) - 1;
+    $randIdFile = rand(0, $max);
+
+    $filePath = __DIR__ . "/img/" . $listFile[$randIdFile];
+
+    $arrayQuery = array(
+        'chat_id' => $chatId,
+        "photo" => new CURLFile($filePath),
+        "caption" => $textMessage_bot
+    );
+    TG_sendPhoto($arrayQuery);
+
+}
+
+/* Функционал по отправке кнопок*/
+
+else if($textMessage == 'отправь кнопки') {
+    $textMessage_bot = "Вот твои кнопки! Нажимай";
+
+    $arrayQuery = array(
+        'chat_id' => $chatId,
+        'text'	=> $textMessage_bot,
+        'parse_mode'	=> "html",
+        'reply_markup' => json_encode(array(
+            'inline_keyboard' => array(
+                array(
+                    array(
+                        'text' => 'Кнопка 1',
+                        'callback_data' => 'but_1',
+                    ),
+
+                    array(
+                        'text' => 'Кнопка 2',
+                        'callback_data' => 'but_2',
+                    )
+                ),
+            ),
+        )),
+    );
+    TG_sendMessage($arrayQuery);
+}
+
+/* Отлавливаем отправленное изображения от пользователя */
+
+if(!empty($documentData)) {
+
+    $arrayQuery = array(
+        "file_id" => $documentData["file_id"],
+    );
+    $resultQuery = TG_getFile($arrayQuery);
+
+    /* записываем ответ в формате PHP массива */
+    $arrDataResult = json_decode($resultQuery, true);
+
+    /* записываем URL необходимого изображения */
+    $fileUrl = $arrDataResult["result"]["file_path"];
+
+    /* формируем полный URL до файла */
+    $photoPathTG = "https://api.telegram.org/file/bot". TG_TOKEN ."/" . $fileUrl;
+
+    /* забираем название файла */
+    $arrFilePath = explode("/", $fileUrl);
+    $newFilerPath = __DIR__ . "/img/" . $arrFilePath[1];
+
+    /* сохраняем файл на сервер */
+    file_put_contents($newFilerPath , file_get_contents($photoPathTG));
+
+    $arrayQuery = array(
+        'chat_id' => $chatId,
+        'text' => "Отличное фото! Я его, пожалуй, сохраню",
+        'parse_mode' => "html",
+    );
+    TG_sendMessage($arrayQuery);
+}
